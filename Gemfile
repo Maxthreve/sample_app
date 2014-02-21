7739e06d3e3d46ad4cc07896a51c4c7d12b05c3a
Maxthreve <Maxthreve@gmail.com>
2014-02-20 22:33:42 -0500

    add a full_title helper

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'rails', '4.0.2'
+gem 'pg' , '0.15.1'
 group :development do
 	gem 'sqlite3' , '1.3.8'
 	gem 'rspec-rails', '2.13.1'
@@ -24,6 +25,5 @@ group :doc do
 end
 
 group :prodction do
-	gem 'pg' , '0.15.1'
 	gem 'rails_12factor' , '0.0.2'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
   
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     
     it "should have the content 'Sample App' " do
@@ -11,8 +12,13 @@ describe "Static Pages" do
   	
     it "should have the right title" do 
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("#{base_title}")
     end   
+    
+    it "should not have a custum page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title(' | Home')
+    end
   end
 
   describe "Help page" do 
@@ -24,7 +30,7 @@ describe "Static Pages" do
   
   	it "should have the right title" do 
   		visit '/static_pages/help'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  		expect(page).to have_title("#{base_title} | Help")
   	end
   end
 
@@ -37,7 +43,24 @@ describe "Static Pages" do
   	
   	it "should have the right title" do 
   		visit '/static_pages/about'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  		expect(page).to have_title("#{base_title} | About Us")
   	end
   end 
+
+  describe "Contact Page" do
+  
+  	it "have the right title" do 
+  		visit '/static_pages/contact'
+  		expect(page).to have_content("Contact")
+  	end
+
+  	it "should have the right title" do 
+  		visit '/static_pages/contact'
+  		expect(page).to have_title("#{base_title} | Contact")
+  	end 
+
+  end
+
+
+
 end
